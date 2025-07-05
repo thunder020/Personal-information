6a59dc55aaf79d8060fe09e5bf40280d389c3852
Thunder ⛈️ <thunder.singin002@gmail.com>
2025-07-06 00:41:39 +0330

    Create Information.py

---
+++ Information.py
@@ -0,0 +1,6 @@
+full_name = input('write your full name:')
+age= int(input('write your age:'))
+height=float(input('write your height:'))
+states = input('write your marital states:')
+future_age= 100-age
+print(f'hello {full_name}.\n you are {age} now and 100 years later,you will be {future_age},\n your height is {height} and your marrital states is {states}')
